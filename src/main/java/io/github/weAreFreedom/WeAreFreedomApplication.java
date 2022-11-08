34fe6f4cfc61084b954a405f99dedfd1c4ff1cb8
park9831.gsitm <park9831.gsitm@gmail.com>
2022-11-08 13:01:06 +0900

    create project

---
+++ src/main/java/io/github/weAreFreedom/WeAreFreedomApplication.java
@@ -0,0 +1,13 @@
+package io.github.weAreFreedom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeAreFreedomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeAreFreedomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/weAreFreedom/WeAreFreedomApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.weAreFreedom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeAreFreedomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
